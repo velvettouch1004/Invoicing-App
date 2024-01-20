693392c29616cfd1d073ed4d53da4d058219abc7
velvettouch1004 <velvettouch1004@gmail.com>
2024-01-20 15:14:13 +0000

    Abstracts away form, starts building

--- components/AddInvoice.tsx
+++ components/AddInvoice.tsx
@@ -25,32 +25,7 @@ import { DialogClose } from "@radix-ui/react-dialog";
 import { toast } from "sonner";
 import { InvoiceForm } from "./InvoiceForm";
 
-export const InvoiceFormSchema = z.object({
-  name: z.string().min(3, { message: "Name must be at least 3 characters" }),
-  email: z.string().email(),
-  address: z
-    .string()
-    .min(3, { message: "Address must be at least 3 characters" }),
-  city: z.string().min(3, { message: "City must be at least 3 characters" }),
-  zip: z.string().min(3, { message: "Zip code must be at least 3 characters" }),
-  country: z
-    .string()
-    .min(3, { message: "Country must be at least 3 characters" }),
-  phone: z.string().min(3),
-  project: z
-    .string()
-    .min(3, { message: "Project must be at least 3 characters" }),
-  item: z.string().min(3, { message: "Item must be at least 3 characters" }),
-  quantity: z.number(),
-  price: z.number(),
-});
-
 export default function AddInvoice() {
-  function onSubmit(data: z.infer<typeof InvoiceFormSchema>) {
-    toast("Invoice saved");
-    console.log(data);
-  }
-
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -58,9 +33,8 @@ export default function AddInvoice() {
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>Add Invoice</DialogHeader>
-        <InvoiceForm onSubmit={onSubmit} />
+        <InvoiceForm />
         <DialogFooter>
-          <Button type="submit">Save</Button>
           <DialogClose asChild>
             <Button>Cancel</Button>
           </DialogClose>
--- components/InvoiceForm.tsx
+++ components/InvoiceForm.tsx
@@ -12,16 +12,16 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Button } from "./ui/button";
-import { DialogClose } from "@radix-ui/react-dialog";
 import { toast } from "sonner";
+import { Input } from "./ui/input";
 
 export const InvoiceFormSchema = z.object({
-  name: z.string().min(3, { message: "Name must be at least 3 characters" }),
-  email: z.string().email(),
+  /* name: z.string().min(3, { message: "Name must be at least 3 characters" }),
+  email: z.string().email(), */
   address: z
     .string()
     .min(3, { message: "Address must be at least 3 characters" }),
-  city: z.string().min(3, { message: "City must be at least 3 characters" }),
+  /* city: z.string().min(3, { message: "City must be at least 3 characters" }),
   zip: z.string().min(3, { message: "Zip code must be at least 3 characters" }),
   country: z
     .string()
@@ -32,35 +32,50 @@ export const InvoiceFormSchema = z.object({
     .min(3, { message: "Project must be at least 3 characters" }),
   item: z.string().min(3, { message: "Item must be at least 3 characters" }),
   quantity: z.number(),
-  price: z.number(),
+  price: z.number(), */
 });
 
-interface InvoiceFormProps {
-  onSubmit: (data: z.infer<typeof InvoiceFormSchema>) => void;
-}
-
-export function InvoiceForm({ onSubmit }: InvoiceFormProps) {
+export function InvoiceForm() {
   const form = useForm<z.infer<typeof InvoiceFormSchema>>({
     resolver: zodResolver(InvoiceFormSchema),
     defaultValues: {
-      name: "",
-      email: "",
+      /* name: "",
+      email: "", */
       address: "",
-      city: "",
+      /* city: "",
       zip: "",
       country: "",
       phone: "",
       project: "",
       item: "",
       quantity: 0,
-      price: 0,
+      price: 0, */
     },
   });
 
+  function onSubmit(data: z.infer<typeof InvoiceFormSchema>) {
+    toast("Invoice saved");
+    console.log(data);
+  }
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}></form>
-      <p>TEST</p>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="address"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="form-label">Street Address</FormLabel>
+              <FormControl>
+                <Input placeholder="19 Union Terrace" {...field} />
+              </FormControl>
+              <FormMessage className="form-message" />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Save</Button>
+      </form>
     </Form>
   );
 }
