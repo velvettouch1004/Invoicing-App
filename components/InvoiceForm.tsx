066752c64d41fcdf0a22cb69492c2f674d77e3b4
velvettouch1004 <velvettouch1004@gmail.com>
2024-01-20 18:17:53 +0000

    Adds further components to form, expands and clarifies schema

--- components/InvoiceForm.tsx
+++ components/InvoiceForm.tsx
@@ -3,6 +3,9 @@
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
 import {
   Form,
   FormControl,
@@ -11,25 +14,57 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Button } from "./ui/button";
 import { toast } from "sonner";
 import { Input } from "./ui/input";
+import { Calendar } from "./ui/calendar";
 
 export const InvoiceFormSchema = z.object({
-  name: z.string().min(3, { message: "Name must be at least 3 characters" }),
-  email: z.string().email(),
-  address: z
+  businessAddress: z
     .string()
     .min(3, { message: "Address must be at least 3 characters" }),
-  city: z.string().min(3, { message: "City must be at least 3 characters" }),
-  zip: z.string().min(3, { message: "Zip code must be at least 3 characters" }),
-  country: z
+  businessCity: z
+    .string()
+    .min(3, { message: "City must be at least 3 characters" }),
+  businessZip: z
+    .string()
+    .min(3, { message: "Postal Code must be at least 3 characters" }),
+  businessCountry: z
     .string()
     .min(3, { message: "Country must be at least 3 characters" }),
-  phone: z.string().min(3),
-  project: z
+  clientName: z
     .string()
-    .min(3, { message: "Project must be at least 3 characters" }),
+    .min(3, { message: "Name must be at least 3 characters" }),
+  clientEmail: z.string().email(),
+  clientAddress: z
+    .string()
+    .min(3, { message: "Address must be at least 3 characters" }),
+  clientCity: z.string().min(3),
+  clientZip: z
+    .string()
+    .min(3, { message: "Postal Code must be at least 3 characters" }),
+  clientCountry: z
+    .string()
+    .min(3, { message: "Country must be at least 3 characters" }),
+  invoiceDate: z.date({ required_error: "Invoice date is required" }),
+  paymentTerms: z.string({
+    required_error: "Please select the payment terms.",
+  }),
+  projectDescription: z
+    .string()
+    .min(3, { message: "Project description must be at least 3 characters" }),
   item: z.string().min(3, { message: "Item must be at least 3 characters" }),
   quantity: z.number(),
   price: z.number(),
@@ -43,14 +78,18 @@ export function InvoiceForm({ onSubmit }: InvoiceFormProps) {
   const form = useForm<z.infer<typeof InvoiceFormSchema>>({
     resolver: zodResolver(InvoiceFormSchema),
     defaultValues: {
-      name: "",
-      email: "",
-      address: "",
-      city: "",
-      zip: "",
-      country: "",
-      phone: "",
-      project: "",
+      businessAddress: "",
+      businessCity: "",
+      businessZip: "",
+      businessCountry: "",
+      clientName: "",
+      clientEmail: "",
+      clientAddress: "",
+      clientCity: "",
+      clientZip: "",
+      clientCountry: "",
+      paymentTerms: "",
+      projectDescription: "",
       item: "",
       quantity: 0,
       price: 0,
@@ -60,9 +99,10 @@ export function InvoiceForm({ onSubmit }: InvoiceFormProps) {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)}>
+        <p>Bill From</p>
         <FormField
           control={form.control}
-          name="address"
+          name="businessAddress"
           render={({ field }) => (
             <FormItem>
               <FormLabel className="form-label">Street Address</FormLabel>
@@ -76,7 +116,7 @@ export function InvoiceForm({ onSubmit }: InvoiceFormProps) {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           <FormField
             control={form.control}
-            name="city"
+            name="businessCity"
             render={({ field }) => (
               <FormItem>
                 <FormLabel className="form-label">City</FormLabel>
@@ -89,7 +129,7 @@ export function InvoiceForm({ onSubmit }: InvoiceFormProps) {
           />
           <FormField
             control={form.control}
-            name="zip"
+            name="businessZip"
             render={({ field }) => (
               <FormItem>
                 <FormLabel className="form-label">Post Code</FormLabel>
@@ -102,7 +142,7 @@ export function InvoiceForm({ onSubmit }: InvoiceFormProps) {
           />
           <FormField
             control={form.control}
-            name="country"
+            name="businessCountry"
             render={({ field }) => (
               <FormItem>
                 <FormLabel className="form-label">Country</FormLabel>
@@ -110,6 +150,142 @@ export function InvoiceForm({ onSubmit }: InvoiceFormProps) {
                   <Input placeholder="United Kingdom" {...field} />
                 </FormControl>
                 <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <p>Bill To</p>
+        <FormField
+          control={form.control}
+          name="clientName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="form-label">Client's Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Alex Grim" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="clientEmail"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="form-label">Client's Email</FormLabel>
+              <FormControl>
+                <Input placeholder="alexgrim@example.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="clientAddress"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="form-label">Client's Address</FormLabel>
+              <FormControl>
+                <Input placeholder="19 Union Terrace" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <FormField
+            control={form.control}
+            name="clientCity"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="form-label">City</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="clientZip"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="form-label">Postal Code</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="clientCountry"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="form-label">Country</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <FormField
+            control={form.control}
+            name="invoiceDate"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="form-label">Invoice Date</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        className={cn(
+                          "w-[240px] pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) =>
+                        date > new Date() || date < new Date("1900-01-01")
+                      }
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="paymentTerms"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="form-label">Payment Terms</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                ></Select>
               </FormItem>
             )}
           />
