0e837de631879e66fdc8263d6d0839709208b398
velvettouch1004 <velvettouch1004@gmail.com>
2024-02-08 18:30:15 +0000

    Refactors InvoiceForm story to incl. args for props

--- stories/InvoiceForm.stories.tsx
+++ stories/InvoiceForm.stories.tsx
@@ -14,4 +14,30 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const InvoiceFormComponent: Story = {};
+export const InvoiceFormComponent: Story = {
+  args: {
+    initialValues: {
+      businessName: "",
+      businessEmail: "",
+      businessAddress: "",
+      businessCity: "",
+      businessZip: "",
+      businessCountry: "",
+      clientName: "",
+      clientEmail: "",
+      clientAddress: "",
+      clientCity: "",
+      clientZip: "",
+      clientCountry: "",
+      invoiceDate: new Date(),
+      paymentTerms: "",
+      paymentDue: "",
+      status: "",
+      projectName: "",
+    },
+    isEditing: false,
+    onSubmit: (data) => {
+      console.log(data);
+    },
+  },
+};
