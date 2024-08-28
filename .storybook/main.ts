8132723619ce40787953214fbd0c5fd8ee406a3d
velvettouch1004 <velvettouch1004@gmail.com>
2024-08-28 21:44:25 +0100

    chore: extend linting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,37 +1,22 @@
-import type { StorybookConfig } from "@storybook/nextjs";
-import path from "path";
+import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
   ],
   framework: {
-    name: "@storybook/nextjs",
+    name: '@storybook/nextjs',
     options: {},
   },
-  staticDirs: ["../public"],
-  async webpackFinal(config, { configType }) {
-    if (config?.resolve?.alias) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        "@/lib": path.resolve(__dirname, "../lib"),
-        "@/components": path.resolve(__dirname, "../components"),
-        "@/stories": path.resolve(__dirname, "../stories"),
-        "@/types": path.resolve(__dirname, "../types"),
-      };
-    }
-    return config;
-  },
-  docs: {
-    autodocs: "tag",
-  },
+  staticDirs: ['../public'],
 };
 export default config;
